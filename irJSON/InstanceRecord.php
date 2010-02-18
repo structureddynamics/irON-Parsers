86b04ecaf8389e152144a7d6e50cf1f4ac77feaa
Frederick Giasson <fred@fgiasson.com>
2010-02-18 18:29:58 +0000

    Version 1.0a2
    Modified
    
        * Making it compliant with the irON specification 0.9
        * Changed the code to make multiple Dataset attribute possible (and not only the core set)

--- irJSON/InstanceRecord.php
+++ irJSON/InstanceRecord.php
@@ -1,241 +1,130 @@
 <?php
 
-/*!	 @brief Instance Record item description
-						
-		\n
-		
-		@todo Enhance setAttributeXRef() to be able to define other MetaData attributes than prefLabel and prefURL.
-		
-		@author Frederick Giasson, Structured Dynamics LLC.
-	
-		\n\n\n
-*/
-class InstanceRecord
-{
-	/*! @brief Local ID of the instance record */				
-	public $id;
-	
-	/*! @brief Type of the instance record */				
-	public $type;
-	
-	/*! @brief Preferred label of the instance record */				
-	public $prefLabel = "";
-	
-	/*! @brief Description of the instance record */				
-	public $description;
-	
-	/*! @brief List of attributes describing the instance record */				
-	public $attributeList = array();
-
-	/*!	 @brief Set the value of the ID
+	/*!	 @brief Instance Record item description
 							
 			\n
-			
-			@param[in] $id ID of the instance record
 			
 			@author Frederick Giasson, Structured Dynamics LLC.
 		
 			\n\n\n
-	*/	
+	*/
+
+class InstanceRecord
+{
+    /*! @brief ID of the instance record. The ID of an instance record is used to create the global reference of local records belonging to the dataset. */                  
+	public $id;
+	
+    /*! @brief All attributes/values describing the dataset */                  
+	public $attributes = array();
+	
+	function __construct(){}
+	
+	function __destruct(){}
+	
+    /*!      @brief Set the value of the ID
+                                                    
+                    \n
+                    
+                    @param[in] $id ID of the dataset
+                    
+                    @author Frederick Giasson, Structured Dynamics LLC.
+            
+                    \n\n\n
+    */    		
 	public function setId($id)
 	{
 		if($id != "")
 		{
 			$this->id = array($id);
 
-			$this->id["valueType"] = "string";
+			$this->id["valueType"] = "primitive:id[1]";
 		}
 	}
 
-	/*!	 @brief Set the value of the type
-							
-			\n
-			
-			@param[in] $type Type of the instance record
-			@param[in] $valueType Value type of the type value (generally "string" or "array(string)")
-			
-			
-			@author Frederick Giasson, Structured Dynamics LLC.
-		
-			\n\n\n
-	*/	
-	public function setType($type, $valueType)
-	{
-		// case unsensitive;
-		//$type = strtolower($type);
-				
-		if($type != "")
-		{	
-			if(!is_array($this->type))
-			{		
-				$this->type = array($type);
-			}
-			else
-			{
-				array_push($this->type, $type);
-			}	
-			
-			$this->type["valueType"] = $valueType;
-		}
-	}
-	
-	/*!	 @brief Set the value of the prefLabel
-							
-			\n
-			
-			@param[in] $prefLabel Preferred label of the dataset
-			@param[in] $valueType Value type of the label value
-			
-			@author Frederick Giasson, Structured Dynamics LLC.
-		
-			\n\n\n
-	*/		
-	public function setLabel($prefLabel, $valueType)
-	{
-		if($prefLabel != "" && $this->prefLabel == "")
-		{		
-			$this->prefLabel = array($prefLabel);
-			
-			$this->prefLabel["valueType"] = $valueType;
-		}
-	}
-
-	/*!	 @brief Set the value of the altLabel
-							
-			\n
-			
-			@param[in] $altLabel Alternative label(s) to refer to this dataset
-			@param[in] $valueType Value type of the label value (normally one of: "string" or "array(string)"
-			
-			@author Frederick Giasson, Structured Dynamics LLC.
-		
-			\n\n\n
-	*/	
-	public function setAltLabel($altLabel, $valueType)
-	{
-		if($altLabel != "")
-		{		
-			if(!is_array($this->altLabel))
-			{		
-				$this->altLabel = array($altLabel);
-			}
-			else
-			{
-				$this->altLabel = array($altLabel);
-			}
-			
-			$this->altLabel["valueType"] = $valueType;
-		}
-	}
-	
-	/*!	 @brief Set the value of the description
-							
-			\n
-			
-			@param[in] $description Description of the dataset
-			
-			@author Frederick Giasson, Structured Dynamics LLC.
-		
-			\n\n\n
-	*/			
-	public function setDescription($description)
-	{
-		if($description != "")
-		{
-			$this->description = array($description);
-
-			$this->description["valueType"] = "string";
-		}
-	}
-	
-	/*!	 @brief Set an attribute/value pairs describing the instance record
-							
-			\n
-			
-			@param[in] $attr Attribute to be added
-			@param[in] $value Value of the attribute
-			@param[in] $valueType Type of the value
-			
-			@author Frederick Giasson, Structured Dynamics LLC.
-		
-			\n\n\n
-	*/			
-	public function setAttributeX($attr, $value, $valueType)
+    /*!      @brief Set a value for an attribute describing the instance record
+                                                    
+                    \n
+                    
+                    @param[in] $attr attribute describing the instance record
+                    @param[in] $value value of the attribute
+                    @param[in] $valueType type of the value of the attribute (ex: String, Object, etc).
+                    
+                    @author Frederick Giasson, Structured Dynamics LLC.
+            
+                    \n\n\n
+    */ 
+	public function setAttribute($attr, $value, $valueType)
 	{
 		if($value != "")
 		{
-			if(!is_array($this->attributeList[$attr]))
+			if(!is_array($this->attributes[$attr]))
 			{		
-				$this->attributeList[$attr] = array($value);
+				$this->attributes[$attr] = array($value);
 			}
 			else
 			{
-				array_push($this->attributeList[$attr], $value);
+				array_push($this->attributes[$attr], $value);
 			}
 			
-			$this->attributeList[$attr]["valueType"] = $valueType;
+			$this->attributes[$attr]["valueType"] = $valueType;
 		}
 	}	
 
-	/*!	 @brief Set an attribute/value pairs describing the instance record, where the value is a reference
-							
-			\n
-			
-			@param[in] $attr Attribute to be added
-			@param[in] $prefLabel MetaData: preferred label of the referred instance record
-			@param[in] $prefURL MetaData: preferred URL of the referred instance record
-			@param[in] $ref Local or global referrence to the instance record
-			@param[in] $valueType Type of the value of the property (usually object or array(object))
-			
-			@author Frederick Giasson, Structured Dynamics LLC.
-		
-			\n\n\n
-	*/			
-	public function setAttributeXRef($attr, $prefLabel, $prefURL, $ref, $valueType)
+    /*!      @brief Set a "ref" attribute
+                                                    
+                    \n
+                    
+                    @param[in] $attr attribute describing the instance record
+                    @param[in] $metaData metaData that describe the <subject, attribute, value> triple.
+                    @param[in] $ref Reference to the local or global ID of the reference
+                    @param[in] $valueType type of the value of the attribute (ex: String, Object, etc).
+                    
+                    @author Frederick Giasson, Structured Dynamics LLC.
+            
+                    \n\n\n
+    */ 
+	public function setAttributeRef($attr, $metaData, $ref, $valueType)
 	{
-		$this->addRef($this->attributeList[$attr], $prefLabel, $prefURL, $ref, $valueType);			
+		$this->addRef($this->attributes[$attr], $metaData, $ref, $valueType);			
 	}
 
-	/*!	 @brief Create a reference to a source, creator, curator or maintenainer of a dataset
-							
-			\n
-			
-			@param[in] $prefLabel MetaData: preferred label of the reference
-			@param[in] $prefURL MetaData: preferred URL of the reference
-			@param[in] $ref Reference to the local or global ID of the reference
-			@param[in] $valueType Value type of the reference value
-			
-			@author Frederick Giasson, Structured Dynamics LLC.
-		
-			\n\n\n
-	*/		
-	private function addRef(&$attr, $prefLabel, $prefURL, $ref, $valueType)
+    /*!      @brief Create a reference to a source, creator, curator or maintenainer of an instance record
+                                                    
+                    \n
+                    
+                    @param[in] $attr attribute reference
+                    @param[in] $metaData metaData that describe the <subject, attribute, value> triple.
+                    @param[in] $ref Reference to the local or global ID of the reference
+                    @param[in] $valueType Value type of the reference value
+                    
+                    @author Frederick Giasson, Structured Dynamics LLC.
+            
+                    \n\n\n
+    */  
+	private function addRef(&$attr, $metaData, $ref, $valueType)
 	{
-		if($prefLabel != "" || $prefURL != "" || $ref != "")
-		{		
-			if(!is_array($attr))
-			{
-				$attr = array(array("prefLabel" => $prefLabel, "prefURL" => $prefURL, "ref" => $ref));
-			}
-			else
-			{
-				array_push($attr, array("prefLabel" => $prefLabel, "prefURL" => $prefURL, "ref" => $ref));
-			}
-			
-			$attr["valueType"] = $valueType;
+		if(!is_array($attr))
+		{
+			$attr = array(array("metaData" => $metaData,"ref" => $ref));
 		}
+		else
+		{
+			array_push($attr, array("metaData" => $metaData,"ref" => $ref));
+		}
+		
+		$attr["valueType"] = $valueType;
 	}
 	
-	/*!	 @brief Get the valueType of an attribute
-							
-			\n
-			
-			@param[in] $property Target property you want to get the valueType
-			
-			@author Frederick Giasson, Structured Dynamics LLC.
-		
-			\n\n\n
-	*/		
+    /*!      @brief Get the valueType of an attribute
+                                                    
+                    \n
+                    
+                    @param[in] $property Target property you want to get the valueType
+                    
+                    @author Frederick Giasson, Structured Dynamics LLC.
+            
+                    \n\n\n
+    */  	
 	public function getValueType($property)
 	{
 		if(isset($this->{$property}["valueType"]))
@@ -244,15 +133,16 @@ class InstanceRecord
 		}
 		else
 		{
-			// Check if it is part of "attributeList"
-			if(isset($this->{"attributeList"}[$property]["valueType"]))
+			// Check if it is part of "attributes"
+			if(isset($this->{"attributes"}[$property]["valueType"]))
 			{
-				return($this->{"attributeList"}[$property]["valueType"]);
+				return($this->{"attributes"}[$property]["valueType"]);
 			}
 
 			return(FALSE);
 		}
 	}
 }
+
 
 ?>
